b4bb4f15d71eb4c0d5d0066e33dc95e27764dfe6
Jamie Jonas <jamie@oneten.tech>
2024-10-02 16:27:52 +0200

    asdvasvasvdsva

--- main.tf
+++ main.tf
@@ -1,5 +1,1 @@
 
-module "dummy" {
-  source = "./modules/module-name"
-  name   = var.name
-}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
-variable "name" {
-  type        = string
-  description = "example string variable"
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "uksouth"
+
 }
